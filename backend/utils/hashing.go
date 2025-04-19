f5d163ea1cdbea84e1bd3559812ef4512ac68e01
W-ptr <wisnu.putra001@binus.ac.id>
2025-04-19 23:34:04 +0700

    first commit, template upload

--- backend/utils/hashing.go
+++ backend/utils/hashing.go
@@ -2,3 +2,13 @@ package utils
 
 import "golang.org/x/crypto/bcrypt"
 
+func HashPassword(plainPassword string) (string, error) {
+	saltRound := 10
+	bytes, err := bcrypt.GenerateFromPassword([]byte(plainPassword), saltRound)
+	return string(bytes), err
+}
+
+func ComparePassword(plainPassword, hashedPassword string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(plainPassword))
+	return err == nil
+}
--- backend/utils/random.go
+++ backend/utils/random.go
@@ -1,1 +1,8 @@
 package utils
+
+import "github.com/google/uuid"
+
+func GenerateUuid() string {
+	randomUuid := uuid.New()
+	return randomUuid.String()
+}
